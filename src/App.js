48d12940c674355857bb253e9d7bba800665b988
Griger Ratia <grigerratia@gmail.com>
2022-09-03 19:11:22 -0400

    Adelantado buena parte de la estructura

--- src/App.js
+++ src/App.js
@@ -1,21 +1,68 @@
 import './App.css';
+import ReactPlayer from 'react-player'
+import video from './assets/videos/360p.mp4'
+import logo from './assets/images/logoStrans.png'
 
 function App() {
   return (
     <div id='principal-container' className='principal-container'>
-      <video src=""></video>
-      <div className='effect-blur'></div>
-      <nav>
-        <div className='logo-principal'></div>
+      <div className='videoContain'>
+
+        <ReactPlayer
+          url={video}
+          playing
+          loop
+          muted
+          width='100%'
+          height='100%'
+          playbackRate={0.4}
+        />
+      </div>
+
+
+      <main>
+
+        {/* <div className='effect-blur'></div> */}
         <nav>
-          <ul>
-            <li>Quienes somos</li>
-            <li>Testimonios</li>
-            <li>galería</li>
-            <li>Contacto</li>
-          </ul>
+          <div className='logo-principal'>
+            <img src={logo} alt="Logo Soy un latino exitoso en USA" />
+          </div>
+            <ul>
+              <li key='nav--quienes-somos'>Quiénes somos</li>
+              <li key='nav--testimonios'>Testimonios</li>
+              <li key='nav--galeria'>Galería</li>
+              <li key='nav--contacto'>Contacto</li>
+            </ul>
         </nav>
-      </nav>
+
+
+
+        <section>
+          <header>
+            <h1>Soy un latino exitoso en USA</h1>
+            <p>Aplica aquí para que veas que es lo bueno</p>
+            <form>
+              <input type="text" />
+              <button type='button'>Aplicar</button>
+            </form>
+            <div className='opcions'>
+              <ul>
+                <li key='form--option1'>Opción 1</li>
+                <li key='form--option2'>Opción 2</li>
+              </ul>
+            </div>
+          </header>
+        </section>
+
+
+        <footer>
+          <div>Redes</div>
+          <div>Dirección</div>
+          <div>Desarrollo</div>
+          <div>Logo</div>
+        </footer>
+      </main>
+     
 
     </div>
   );
